786e8fc42206e7ce6a49b4af1e8dbeadea600646
garyburke888 <garyburke888@gmail.com>
2020-10-28 17:22:21 +0000

    Create the Flask App

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "]!4@8]r8*-[A,/BJK%Xt5cu.3Xh$~f")
+os.environ.setdefault("MONGO_URI", "")
+os.environ.setdefault("MONGO_DBNAME", "risk_manager")
