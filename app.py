851a70796820c3652296582827955a008aa3f510
garyburke888 <garyburke888@gmail.com>
2020-10-31 09:37:33 +0000

    Create add_risk template

--- app.py
+++ app.py
@@ -98,6 +98,11 @@ def logout():
     session.pop("user")
     return redirect(url_for("login"))
 
+
+@app.route("/add_risk")
+def add_risk():
+    return render_template("add_risk.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
