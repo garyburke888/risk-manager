f18e99292d66c743635bb503f0c0c212453b2fa6
garyburke888 <garyburke888@gmail.com>
2021-01-06 16:16:59 +0000

    Remove env.py file

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "]!4@8]r8*-[A,/BJK%Xt5cu.3Xh$~f")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://root:r00tUser@myfirstcluster.mxbol.mongodb.net/risk_manager?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "risk_manager")
